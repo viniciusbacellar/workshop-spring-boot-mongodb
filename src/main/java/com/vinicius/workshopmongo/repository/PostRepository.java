74ac9f0667c9539592c1d1e03346bcfe4be2fd17
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-03-22 16:55:56 -0300

    Consulta simples com query methods

--- src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.vinicius.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.vinicius.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/vinicius/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vinicius/workshopmongo/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.vinicius.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.workshopmongo.domain.Post;
+import com.vinicius.workshopmongo.resources.util.URL;
 import com.vinicius.workshopmongo.services.PostService;
 
 @RestController
@@ -23,4 +27,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(obj);
 	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.status(HttpStatus.OK).body(list);
+	}
+
 }
